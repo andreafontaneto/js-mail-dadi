699ae2a0b4480e9d307dad638c5ecdec6ac66580
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-26 16:52:25 +0200

    consegna esercizio + passaggi logici fix + creazione costanti + ciclo if

--- es dadi/js/script.js
+++ es dadi/js/script.js
@@ -1,19 +1,49 @@
 //Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 /*
-1- creare un elenco di elementi (da 1 a 6)
-    - indicizzare elenco tramite ciclo FOR
-2- creare 2 variabili (giocata UTENTE e giocata COMPUTER)
-3- tramite Math.random far pescare un numero a caso dall'elenco
-4- creare un ciclo IF per decidere qual'è il numero più alto
+- creare 2 variabili (giocata UTENTE e giocata COMPUTER)
+- far pescare un numero intero a caso da 1 a 6 tramite (Math.round(Math.random()*5)+1)
+- creare un ciclo IF per decidere qual'è il numero più alto
 */
 
-const dado = ['1',
-'2',
-'3',
-'4',
-'5',
-'6',
-];
 
-console.log(dado);
+let giocataUtente = (Math.round(Math.random()*5)+1);
+console.log('giocata utente',giocataUtente);
+
+let giocataComputer = (Math.round(Math.random()*5)+1);
+console.log('giocata computer',giocataComputer);
+
+const play = document.getElementById('play');
+
+if(giocataUtente == giocataComputer){
+  console.log('Pareggio');
+  
+  play.innerHTML =
+  `
+  La tua giocata è ${giocataUtente}<br>
+  La giocata del computer è ${giocataComputer}<br>
+  ${giocataUtente} è uguale a ${giocataComputer}<br>
+  Pareggio!
+  `;
+} else if(giocataUtente > giocataComputer){
+  console.log('Utente vince');
+  
+  play.innerHTML =
+  `
+  La tua giocata è ${giocataUtente}<br>
+  La giocata del computer è ${giocataComputer}<br>
+  ${giocataUtente} è maggiore di ${giocataComputer}<br>
+  Complimenti! Hai vinto!
+  `;
+} else {
+  console.log('Computer vince');
+  
+  play.innerHTML =
+  `
+  La tua giocata è ${giocataUtente}<br>
+  La giocata del computer è ${giocataComputer}<br>
+  ${giocataUtente} è minore di ${giocataComputer}<br>
+  Mi spiace! Hai perso!
+  `;
+};
+
