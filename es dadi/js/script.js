395b4225ab560706eb3b485a56ee3c65c37481ef
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-26 15:51:18 +0200

    consegna esercizio + passaggi logici

--- es dadi/js/script.js
+++ es dadi/js/script.js
@@ -1,1 +1,9 @@
-console.log('ciao');
+//Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+/*
+1- creare un elenco di elementi (da 1 a 6)
+2- creare 2 variabili (giocata UTENTE e giocata COMPUTER)
+3- tramite Math.random far pescare un numero a caso dall'elenco
+4- creare un ciclo IF per decidere qual'è il numero più alto
+*/
+
