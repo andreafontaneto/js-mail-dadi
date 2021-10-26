19432751176513b1d824b6faf00fa9b244588db2
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-26 14:50:36 +0200

    consegna esercizio + passaggi logici

--- es email/js/script.js
+++ es email/js/script.js
@@ -1,1 +1,16 @@
-console.log('ciao');
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,stampa un messaggio appropriato sull’esito del controllo.
+
+/*
+1- creare un array di email
+2- inizializzo una costante di default NON TROVATA (false)
+3- dichiaro un prompt
+4- inizializzo ciclo FOR per cercare l'email inserita
+5- dentro a FOR inserisco un IF:
+      - SE l'email inserita è uguale a un email nella lista --> risultato = email trovata
+6- quando la ricerca è stata terminata dichiaro un altro ciclo IF:
+      - SE risultato = email trovata, allora dichiaro che è stata trovata
+      - ALTRIMENTI dichiaro che NON è stata trovata
+*/
+
+
+
