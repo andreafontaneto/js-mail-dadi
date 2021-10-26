0e37b7184d633865c4c22ef036d623ad4e337108
andreafontaneto <andrea.fontaneto@gmail.com>
2021-10-26 16:02:09 +0200

    creazione elenco dado

--- es dadi/js/script.js
+++ es dadi/js/script.js
@@ -2,8 +2,18 @@
 
 /*
 1- creare un elenco di elementi (da 1 a 6)
+    - indicizzare elenco tramite ciclo FOR
 2- creare 2 variabili (giocata UTENTE e giocata COMPUTER)
 3- tramite Math.random far pescare un numero a caso dall'elenco
 4- creare un ciclo IF per decidere qual'è il numero più alto
 */
 
+const dado = ['1',
+'2',
+'3',
+'4',
+'5',
+'6',
+];
+
+console.log(dado);
